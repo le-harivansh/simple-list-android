eac139892008e3800c471b86d728aa4691c8c77d
Harivansh <hello@harivan.sh>
2023-06-25 13:14:38 +0400

    refactor: add edit & delete icons

--- app/src/main/java/com/example/simplelist/ui/composables/ListItem.kt
+++ app/src/main/java/com/example/simplelist/ui/composables/ListItem.kt
@@ -1,17 +1,23 @@
 package com.example.simplelist.ui.composables
 
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import com.example.simplelist.R
 import com.example.simplelist.model.Item
 
 @Composable
@@ -33,13 +39,17 @@ fun ListItem(item: Item, deleteItem: (Int) -> Unit, navigateToItem: (Int) -> Uni
         }
 
         Row(modifier = Modifier.padding(4.dp)) {
-            Button(onClick = { navigateToItem(item.id) }) {
-                Text(text = "i")
-            }
+            Icon(
+                painter = painterResource(id = R.drawable.ic_pen),
+                contentDescription = "Edit item icon",
+                modifier = Modifier.clickable { navigateToItem(item.id) }
+            )
 
-            Button(onClick = { deleteItem(item.id) }) {
-                Text(text = "X")
-            }
+            Icon(
+                painter = painterResource(id = R.drawable.ic_trash),
+                contentDescription = "Delete item icon",
+                modifier = Modifier.clickable { deleteItem(item.id) }
+            )
         }
     }
 }
