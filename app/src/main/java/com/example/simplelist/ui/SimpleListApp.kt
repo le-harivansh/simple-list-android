0951e830aab69e775f6315ce03d722cb58fbd039
Harivansh <hello@harivan.sh>
2023-06-25 10:52:25 +0400

    feat: add FAB to home screen

--- app/src/main/java/com/example/simplelist/ui/SimpleListApp.kt
+++ app/src/main/java/com/example/simplelist/ui/SimpleListApp.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -25,7 +26,12 @@ fun SimpleListApp() {
 
     Scaffold(
         modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
-        topBar = { TopAppBar(scrollBehavior = scrollBehavior) }
+        topBar = { TopAppBar(scrollBehavior = scrollBehavior) },
+        floatingActionButton = {
+            FloatingActionButton(onClick = { /*TODO*/ }) {
+                Text(text = "+")
+            }
+        }
     ) {
         Surface(modifier = Modifier
             .fillMaxSize()
