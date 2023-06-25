f180b1aabdffa12ad2b14a3cb0314f5f02ba300d
Harivansh <hello@harivan.sh>
2023-06-25 08:08:42 +0400

    feat: add item model

--- app/src/main/java/com/example/simplelist/model/Item.kt
+++ app/src/main/java/com/example/simplelist/model/Item.kt
@@ -1,6 +1,12 @@
 package com.example.simplelist.model
 
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class Item(
+    @SerialName("id")
     val id: Int,
+    @SerialName("title")
     val title: String
 )
